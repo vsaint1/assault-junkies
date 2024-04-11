c5a51f22a5649562b95fd4e8911a73406f955a5c
vsaint1 <vsantos1.dev@gmail.com>
2024-04-11 00:58:55 -0300

    hooking and calling wgl

---
+++ src/hook/hook.cpp
@@ -0,0 +1,40 @@
+#include "hook.h"
+
+typedef BOOL(__stdcall *WglSwapBuffers)(HDC hdc);
+
+WglSwapBuffers o_swap_buffers = nullptr;
+
+
+BOOL hk_swap_buffers(HDC hdc) {
+
+	render->ortho();
+	render->build_font();
+    render->draw_text(200, 250, WHITE, "%s %d", "abcdefghijk", 1234);
+    spdlog::info("Calling original swapbuffers");
+
+	render->restore();
+    return o_swap_buffers(hdc);
+}
+
+bool hook::setup() {
+
+    MH_Initialize();
+
+    if (MH_CreateHookApi(L"OpenGL32.dll", "wglSwapBuffers", &hk_swap_buffers, reinterpret_cast<void **>(&o_swap_buffers)) != MH_OK)
+        return false;
+
+    if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK)
+        return false;
+
+    spdlog::info("Hooks done");
+
+    return true;
+}
+
+bool hook::shutdown() {
+    MH_DisableHook(MH_ALL_HOOKS);
+
+    MH_Uninitialize();
+
+    return true;
+}
---
+++ src/hook/hook.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../utils/macros.h"
+#include "../opengl/render.h"
+
+namespace hook {
+
+bool setup();
+
+bool shutdown();
+
+} // namespace hook
